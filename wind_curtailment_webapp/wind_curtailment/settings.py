50de02cbea2a8ed9d96cc93536153649c694b8b2
Marie Gondeck <MarieGondeck@live.de>
2024-02-29 18:51:26 +0100

    included first table in webapp

--- wind_curtailment_webapp/wind_curtailment/settings.py
+++ wind_curtailment_webapp/wind_curtailment/settings.py
@@ -55,7 +55,7 @@ ROOT_URLCONF = 'wind_curtailment.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [BASE_DIR / 'myapp' / 'templates'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
